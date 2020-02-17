2919c62370dff20fa7d7c6393268deac668b7475
shake <shakes150e@gmail.com>
2020-02-17 18:01:11 +0200

    initial commit

---
+++ src/com/compay/Main.java
@@ -0,0 +1,10 @@
+package com.compay;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Shakes150 !!");
+        System.out.println("Still not REGISTERED in 2020");
+    }
+}
